c433f0db467d9dbc907593ac718af8328cc7beaf
basoo21sook-cpu <basoo21sook@gmail.com>
2025-11-06 15:31:54 +0900

    10-4

--- main.c
+++ main.c
@@ -3,12 +3,10 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+
 void main(void) {
-    char src[] = "The worst things to eat before you sleep";
-    char dst[100];
+    char str[30] = "happy C programming";
 
-    strcpy(dst, src); // <--- 문자열 복사 (dst = 대상, src = 원본)
-
-    printf("copied string : %s\n", dst);
+    printf("문자열 \"%s\"의 길이 : %i\n", str, strlen(str));
 }
 
