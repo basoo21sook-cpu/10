50e5178cde9e5eae2d100ed66afa87c40e4ae270
basoo21sook-cpu <basoo21sook@gmail.com>
2025-11-06 15:43:25 +0900

    10-5

--- main.c
+++ main.c
@@ -1,12 +1,26 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int main(void) {
+    FILE *fp;        
+    char input[100]; 
+    int i;
 
-void main(void) {
-    char str[30] = "happy C programming";
-
-    printf("문자열 \"%s\"의 길이 : %i\n", str, strlen(str));
+   //fopen
+    fp = fopen("sample.txt", "w");
+	
+	for (i = 0; i < 3; i++) {
+	//fprintf 
+    printf("input a word: ");
+    scanf("%s", input);
+    fprintf(fp, "%s\n", input); 
 }
+	//fclose
+    fclose(fp); 
+    return 0;
+}
+
 
